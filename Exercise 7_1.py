c923498229b97881da31a2debc895d08a9239a3d
cdmarks <37555254+cd-marks@users.noreply.github.com>
2020-08-21 14:05:02 -0700

    Delete Exercise 7_1.py

--- Exercise 7_1.py
+++
@@ -1,7 +0,0 @@
-# Write a program to read through a file and print the contents of the file, line by line, all in upper case
-
-filename = input('Enter the name of a file: ') # Make sure to include the full directory, too!
-fhand = open(filename)
-for line in fhand:
-    line = line.rstrip()
-    print(line.upper())
